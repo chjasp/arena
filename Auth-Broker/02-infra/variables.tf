d9eb71888409195f064e48a2816f82456e67c203
chjasp <ch.jasper1@gmail.com>
2025-09-24 09:44:01 +0200

    Initial commit

---
+++ Auth-Broker/02-infra/variables.tf
@@ -0,0 +1,10 @@
+variable "project_id"        { type = string }
+variable "region"            { type = string }
+variable "service_name"      { type = string }
+variable "image_url"         { type = string }
+
+variable "firestore_location_id" { type = string }
+
+variable "whoop_client_id"     { type = string }
+variable "whoop_client_secret" { type = string }
+variable "broker_api_key"      { type = string }
